1514121b1fc92e08ea04a5c38f253c40d31bf392
Rodrigo G Ramirez M <jadwer@msn.com>
2021-09-27 13:53:32 -0500

    Config Header

---
+++ jwr-alea-crm.php
@@ -0,0 +1,22 @@
+/**
+ * JwR-Alea-CRM
+ *
+ * @package           AleaCRM
+ * @author            Gabino Ramírez
+ * @copyright         2019 AtomoSoluciones
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       JwR Alea CRM
+ * Plugin URI:        https://plugins.gabinoramirez.com/alea
+ * Description:       This Plugin is an update from a Joomla CRM to Wordpress.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.4
+ * Author:            Gabino Ram´   
+ * Author URI:        https://www.gabinoramirez.com/alea
+ * Text Domain:       jwr-alea-crm
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://plugins.gabinoramirez.com/alea/jwr-alea-crm/
+ */
