66c9e26899822b5c06b0dbcebfe57d6ac17a0be5
Rodrigo G Ramirez M <jadwer@msn.com>
2021-09-29 16:08:49 -0500

    DB tables created and deleted from plugin activation 2

--- classes/AleaModel.php
+++ classes/AleaModel.php
@@ -34,6 +34,60 @@ class AleaModel
         if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
             $wpdb->query($query);
         }
+
+        $table_name = $wpdb->prefix."alea_dietas";
+
+        $query = "
+        CREATE TABLE IF NOT EXISTS `{$table_name}` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `cliente` int NOT NULL,
+            `nif` varchar(20) NOT NULL DEFAULT '',
+            `tipo` tinyint(1) NOT NULL COMMENT 'nueva o continua',
+            `fecha` datetime NOT NULL,
+            `parametros` mediumtext NOT NULL,
+            `state` tinyint NOT NULL,
+            `order` varchar(13) NOT NULL DEFAULT '',
+            `enviado` tinyint NOT NULL DEFAULT '0',
+            `opc` tinyint NOT NULL DEFAULT '0',
+            `recordar` tinyint NOT NULL DEFAULT '0',
+            `tipoDieta` tinyint NOT NULL DEFAULT '0',
+            `nuevoModelo` tinyint NOT NULL DEFAULT '1',
+            PRIMARY KEY (`id`)
+          ) ENGINE = InnoDB AUTO_INCREMENT = 9988 DEFAULT CHARSET = utf8mb3;
+              ";
+        if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+            $wpdb->query($query);
+        }
+
+        $table_name = $wpdb->prefix."alea_facturas";
+
+        $query = "
+        CREATE TABLE IF NOT EXISTS `{$table_name}` (
+            `id` int NOT NULL AUTO_INCREMENT,
+            `referencia` varchar(15) NOT NULL,
+            `fecha` datetime NOT NULL,
+            `cliente` int NOT NULL,
+            `dietaid` int NOT NULL,
+            `nombre` varchar(50) NOT NULL,
+            `apellidos` varchar(100) NOT NULL,
+            `nif` varchar(20) NOT NULL,
+            `calle` varchar(200) NOT NULL,
+            `numero` smallint NOT NULL,
+            `pisoLetra` varchar(5) NOT NULL,
+            `cp` varchar(5) NOT NULL,
+            `ciudad` varchar(50) NOT NULL,
+            `provincia` varchar(50) NOT NULL,
+            `concepto` varchar(255) NOT NULL DEFAULT '',
+            `precio` float(5, 2) NOT NULL,
+            `iva` float(5, 2) NOT NULL DEFAULT '0.00',
+            `total` float(5, 2) NOT NULL,
+            `state` tinyint NOT NULL,
+            PRIMARY KEY (`id`)
+          ) ENGINE = InnoDB AUTO_INCREMENT = 28114 DEFAULT CHARSET = utf8mb3;
+              ";
+        if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+            $wpdb->query($query);
+        }
     }
 
     public static function deleteTables()
@@ -46,7 +100,22 @@ class AleaModel
             DROP TABLE {$table_name};
               ";
             $wpdb->query($query);
-    }
+
+            $table_name = $wpdb->prefix."alea_dietas";
+
+            $query = "
+                DROP TABLE {$table_name};
+                  ";
+                $wpdb->query($query);
+    
+                $table_name = $wpdb->prefix."alea_facturas";
+
+                $query = "
+                    DROP TABLE {$table_name};
+                      ";
+                    $wpdb->query($query);
+        
+                    }
 
     public static function test()
     {
